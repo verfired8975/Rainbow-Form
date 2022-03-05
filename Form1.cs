fb28172a56ca623b8da177daef1d18112364d0a7
Arda <96637948+verfired8975@users.noreply.github.com>
2022-03-06 00:51:27 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qweqew
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int r = 255, g = 0, b = 0;
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+             
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Stop();
+                
+                this.checkBox1.ForeColor = Color.Red;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.checkBox1.ForeColor = Color.FromArgb(r, g, b);
+            if (r > 0 && b == 0)
+            {
+                r--;
+                g++;
+            }
+            if (g > 0 && r == 0)
+            {
+                g--;
+                b++;
+            }
+            if (b > 0 && g == 0)
+            {
+                b--;
+                r++;
+            }
+        }
+    }
+           }
+
